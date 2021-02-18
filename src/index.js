8b629d54c81a270c3ac0550d00ac451e07e81613
Fernando <fernando.chaves16@hotmail.com>
2021-02-18 17:50:11 -0300

    uuidv4 library added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,55 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3333;
 
-//app.use(express.json());
+// Falar para o Express que a Api vai receber informações no formato JSON
+app.use(express.json());
+
+const pokemons = [];
 
 // get
-app.get('/', (req, res) => {
-  res.send('Primeiro server Node')
+app.get('/Pokemons', (req, res) => {
+  const {name, type} = req.query;
+
+  //console.log(name);
+  //console.log(type);
+
+  return res.json(pokemons);
 });
 
 // post
+app.post('/Pokemons', (req, res) => {
+  const {name, type} = req.body;
+
+  const pokemon = {name, type}
+
+  console.log(name);
+  console.log(type);
+
+  return res.json([
+    'Pokemon 4',
+    'Pokemon 5',
+    'Pokemon 6'
+  ])
+})
 
 // put
+app.put('/Pokemons/:id', (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
+
+  return res.json([
+    'Pokemon 4',
+    'Pokemon 5',
+    'Pokemon 6'
+  ])
+});
 
 // delete
+app.delete('/Pokemons/:id', (req, res) => {
+
+});
 
 
 app.listen(port, () => {
