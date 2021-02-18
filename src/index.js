64e8ec54efb8ed479b4d12fcc1a44bca874cbe32
Fernando <fernando.chaves16@hotmail.com>
2021-02-18 04:31:20 -0300

    server started

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const express = require('express');
-
 const app = express();
+const port = 3000;
 
-app.use(express.json());
+//app.use(express.json());
+
+// get
+app.get('/', (req, res) => {
+  res.send('Primeiro server Node')
+});
+
+// post
+
+// put
+
+// delete
+
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+});
