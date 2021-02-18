4a7f55b9afc071683c4bb2a4d7a602316883a31e
Fernando <fernando.chaves16@hotmail.com>
2021-02-18 18:24:12 -0300

    finished api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 const app = express();
 const port = 3333;
 
@@ -9,46 +10,68 @@ const pokemons = [];
 
 // get
 app.get('/Pokemons', (req, res) => {
-  const {name, type} = req.query;
+  const {type} = req.query;
 
-  //console.log(name);
-  //console.log(type);
+  // Verificar se o titulo foi preenchido pelo usuario;
+  const results = type
+    ? pokemons.filter(pokemon => pokemon.type.includes(type))
+    : pokemons;
 
-  return res.json(pokemons);
+  return res.json(results);
 });
 
 // post
 app.post('/Pokemons', (req, res) => {
   const {name, type} = req.body;
 
-  const pokemon = {name, type}
+  const pokemon = {id: uuid(), name, type}
 
-  console.log(name);
-  console.log(type);
+  pokemons.push(pokemon);
 
-  return res.json([
-    'Pokemon 4',
-    'Pokemon 5',
-    'Pokemon 6'
-  ])
+  return res.json(pokemon);
 })
 
 // put
 app.put('/Pokemons/:id', (req, res) => {
   const { id } = req.params;
+  const {name, type} = req.body;
 
-  console.log(id);
+  // Procurar Pokemon no array
+  const pokemonIndex = pokemons.findIndex(pokemon => pokemon.id === id);
 
-  return res.json([
-    'Pokemon 4',
-    'Pokemon 5',
-    'Pokemon 6'
-  ])
+  // Se ele não existir: 
+  if(pokemonIndex < 0) {
+    return res.status(400).json({ error: 'Pokemon not found.' })
+  }
+
+  // Se ele existir:
+  const pokemon = {
+    id,
+    name, 
+    type
+  }
+
+  pokemons[pokemonIndex] = pokemon;
+
+  return res.json(pokemon)
 });
 
 // delete
 app.delete('/Pokemons/:id', (req, res) => {
+  const { id } = req.params;
 
+  // Procurar Pokemon no array:
+  const pokemonIndex = pokemons.findIndex(pokemon => pokemon.id === id);
+
+  // Se ele não existir:
+  if(pokemonIndex < 0) {
+    return res.status(400).json({ error: 'Pokemon not found.' })
+  }
+
+  // Se ele existir:
+  pokemons.splice(pokemonIndex, 1);
+
+  return res.status(204).send();
 });
 
 
